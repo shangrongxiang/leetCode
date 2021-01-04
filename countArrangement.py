5af7e4359f78b5266fe782759d511904ffcc5ba1
Shang Rongxiang <1074482757@qq.com>
2021-01-04 23:00:39 +0100

    f

---
+++ countArrangement.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+for i in range(n):
+    item = np.random(.randint(1,n+1))
+    
+
+
+print(np.random.randint(1, 10))
+def countArrangement(n):
+    for i in range(n):
+        list1 = np.random.randint(10, 10)
+
+    (i + 1) % pre[i]== 0
+    pre[i] % (i + 1) == 0
